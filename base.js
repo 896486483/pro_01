46cd864e5ba45fed68a08261eb6433ce8c6b3d55
xwjian <896486483@qq.com>
2022-09-15 14:41:54 +0800

    增加提示框

--- base.js
+++ base.js
@@ -16,3 +16,19 @@ axios.interceptors.response.use(response => {
     NProgress.done()
     return Promise.reject(error);
 });
+
+// 3 设置 提示框
+toastr.options = {
+    closeButton: true, // 显示 关闭按钮
+    // debug: false,
+    positionClass: 'toast-top-center', // 提示框显示的位置
+    // onclick: null,
+    // showDuration: '300',
+    // hideDuration: '1000',
+    // timeOut: '5000',
+    // extendedTimeOut: '1000',
+    // showEasing: 'swing',
+    // hideEasing: 'linear',
+    // showMethod: 'fadeIn',
+    // hideMethod: 'fadeOut',
+};
